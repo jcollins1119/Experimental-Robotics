448313ecbdb1781e26289b6b1f5bcf2d7d0d1887
jcollins1119 <jcollins1119@gmail.com>
2019-10-13 22:11:52 -0400

    Add files via upload

--- joy_teleop.py
+++ joy_teleop.py
@@ -8,8 +8,8 @@ vy = 0
 
 def joy_callback(data):
     global vx, vy
-    vx = data.axes[2]
-    vy = data.axes[3]
+    vx = data.axes[3]
+    vy = data.axes[4]
     rospy.loginfo("x: %s", vx)
     rospy.loginfo("y: %s", vy)
 
@@ -19,14 +19,14 @@ def imu_callback(data):
 
 def main():
     rospy.init_node('joy_teleop', anonymous=True)
-    vel_pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
+    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 #    rospy.Subscriber("/mobile_base/sensors/imu_data", Imu, imu_callback)
     rospy.Subscriber("/joy", Joy, joy_callback)
     rate = rospy.Rate(10)
     vel = Twist()
     while not rospy.is_shutdown():
-        vel.linear.x = .5*vy
-        vel.angular.z = 2*(vx if vy >=0 else -vx)
+        vel.linear.x = vy
+        vel.angular.z = (vx if vy >=0 else -vx)
         vel_pub.publish(vel)
         rate.sleep()
 
